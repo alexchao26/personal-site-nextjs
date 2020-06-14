add75ac9e6a6c68a0a2de9a516eb78dc19061196
alexchao26 <alexchaoo@gmail.com>
2020-06-14 09:25:37 -0400

    added BP for projects page

---
+++ content/projects.js
@@ -0,0 +1,31 @@
+export default [
+  {
+    name: 'GraphQuill',
+    description: 'GraphQL DevTool to query your API from VSCode',
+    github: 'https://github.com/oslabs-beta/GraphQuill',
+    website: 'https://marketplace.visualstudio.com/items?itemName=sproutdeveloping.graphquill',
+    githubOwner: 'oslabs-beta',
+    githubRepo: 'GraphQuill',
+  }, {
+    name: 'Dockerized GraphQL API',
+    description: 'A Dockerized GraphQL API made during development of GraphQuill',
+    github: 'https://github.com/GraphQuill/Mock-GraphQL-API-Docker',
+    // website: 'NA',
+    githubOwner: 'GraphQuill',
+    githubRepo: 'Mock-GraphQL-API-Docker',
+  }, {
+    name: 'SocketMan',
+    description: 'Multiplayer Hangman with WebSockets',
+    github: 'https://github.com/alexchao26/SocketMan/',
+    website: 'https://socket-man.herokuapp.com/',
+    githubOwner: 'alexchao26',
+    githubRepo: 'SocketMan',
+  }, {
+    name: 'Personal (this!) Site',
+    description: 'My personal (this) website, built in NextJS',
+    github: 'https://github.com/alexchao26/personal-site-nextjs',
+    website: 'https://alexchao.dev',
+    githubOwner: 'alexchao26',
+    githubRepo: 'personal-site-nextjs',
+  },
+];
---
+++ pages/projects.js
@@ -0,0 +1,10 @@
+import projectsContent from '@content/projects';
+import styles from '@styles/pages/projects.module.scss';
+
+export default () => (
+  <div className={styles.container}>
+    {projectsContent.map(({ name }) => (
+      <p>{name}</p>
+    ))}
+  </div>
+);
