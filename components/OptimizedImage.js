917e196ae8294d63e4ae1ad66ca5f214cb84ee6d
alexchao26 <alexchaoo@gmail.com>
2020-06-16 12:15:15 -0400

    load blurred images in as placeholder

--- components/OptimizedImage.js
+++ components/OptimizedImage.js
@@ -1,18 +1,40 @@
+/* eslint-disable global-require, import/no-dynamic-require */
 import React from 'react';
 
 export default ({ src, alt, className }) => (
   /\.svg$/i.test(src)
     ? <img src={`/images/${src}`} alt={alt} className={className} />
     : (
-      <div>
-        <img
-    // eslint-disable-next-line global-require, import/no-dynamic-require
-          src={require(`../images/${src}`)}
-          alt={alt}
-          className={className}
-        />
-        {/* <img src={require('./images/my-small-image.png')} />
-  <img src={require('./images/my-icon.svg')} /> */}
+      <div className={className}>
+        <div className="image-container">
+          <img
+            src={require(`../images/${src}?lqip`)}
+            alt={alt}
+            className={`blur-image ${className}`}
+          />
+          <img
+            src={require(`../images/${src}`)}
+            alt={alt}
+            className={className}
+          />
+
+
+        </div>
+        <style jsx>
+          {`
+          .image-container {
+            position: relative;
+          }
+          .blur-image img {
+            blur(25px);
+          }
+          img {
+            position: absolute;
+            top: 0;
+            left: 0;
+          }
+          `}
+        </style>
       </div>
     )
 );
